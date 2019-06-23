7000326bddddf38f05417fee0dca07626fa3d08d
neb <neb>
2019-06-23 13:20:46 -0400

    added automatic resizer and font setter for end of setup.sh - first checks if gsettings command is available

--- setup.sh
+++ setup.sh
@@ -49,4 +49,13 @@ else
     cd -
 fi
 
-    
+#gnome-terminal settings update
+if [ -f "$(which gsettings)" ]; then
+    echo "Update terminal config settings for DejaVu Sans Mono 9? Need 'Yes' to confirm: "
+    read input
+    if [[ $input == 'Yes' ]]; then
+        profile=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+        gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" font 'DejaVu Sans Mono 9'
+        gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" use-system-font false
+    fi
+fi
